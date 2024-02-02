4a44f0907037435855bda2c0049eaccca9f90e98
SongheeKim <kkbuh2@gmail.com>
2024-02-02 17:47:03 +0900

    240202 퇴근

---
+++ studywithme/4659_bj_240202.py
@@ -0,0 +1,15 @@
+import re
+
+password = input()
+
+for vowel in ['a', 'e', 'i', 'o', 'u']:
+    if vowel not in password:
+        result = no_acceptable
+
+jaeums = re.split('a|e|i|o|u', password)
+print(jaeums)
+
+for jaeum in jaeums:
+    if len(jaeum) >= 3:
+        result = no_acceptable
+
