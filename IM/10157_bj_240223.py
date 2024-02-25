4351bdef52be0d1ab71f417ff6c22592610a7639
kkbuh2 <kkbuh2@gmail.com>
2024-02-25 21:47:54 +0900

    240225 맥북

--- IM/10157_bj_240223.py
+++ IM/10157_bj_240223.py
@@ -1,24 +1,15 @@
 C, R = map(int, input().split())   # 가로 C, 세로 R / (x, y)
 K = int(input())
 
-if C * R < K:
+if C * R < K:   # 주어진 번호가 좌석 수보다 클 때
     print(0)
-else:
+else:           # 주어진 번호가 좌석 수 이하일 때
+    hall = [[0] * C for _ in range(R + 1)]    # 공연장 설정
 
-    hall = [[0] * C for _ in range(R + 1)]
-
-
-
-    dx = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+    dt = [[-1, 0], [0, 1], [1, 0], [0, -1]]   # i, j 방향 설정 변수 설정
     i = R
     j = 0
-    hall[i][j] = 1
-    while K > 0:                            # 주어진 수만큼 관객을 앉힌다.
-        for k in range(4):
-            while (0 <= i + dx[k][0] < R) and (0 <= j + dx[k][1] < C):
-                i += dx[k][0]
-                j += dx[k][1]
-                hall[i][j] = 1
-                K -= 1
+
+
 
     print(j, i)
