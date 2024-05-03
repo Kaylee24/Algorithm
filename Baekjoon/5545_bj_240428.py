d8751c1274a7ba65443de9d5ccfb88e7161126c2
Kaylee <kkbuh2@gmail.com>
2024-05-03 21:44:30 +0900

    240503 정리

--- Baekjoon/5545_bj_240428.py
+++ Baekjoon/5545_bj_240428.py
@@ -1,16 +1,23 @@
-'20:59 - 21:24, 18:07 - 18:37'
+'[Python3 31120KB, 40ms | 108080KB 104ms]'
 
 import sys
 input = sys.stdin.readline
 
-N = int(input())                             # 토핑의 종류의 수 N
-A, B = map(int, input().split())             # 도우의 가격 A, 토핑의 가격 B
-C = int(input())                             # 도우의 열량 C
-topping = [int(input()) for _ in range(N)]   # 각 토핑의 열량
+N = int(input())                              # 토핑의 종류의 수 N
+A, B = map(int, input().split())              # 도우의 가격 A, 토핑의 가격 B
+C = int(input())                              # 도우의 열량 C
+toppings = [int(input()) for _ in range(N)]   # 각 토핑의 열량
 
+toppings.sort(reverse=True)
+pizza, price = C, A
+best_pizza = pizza / price
+for topping in toppings:
+    pizza += topping
+    price += B
+    if best_pizza < pizza / price:
+        best_pizza = pizza / price
 
-
-
+print(int(best_pizza))
 
 
 
