95657cb6016f0759aa284c5f4304ea40b9bc7bed
Songhee Kim <156345314+Kaylee24@users.noreply.github.com>
2025-01-07 03:16:36 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 404 ms, Memory: 150752 KB -BaekjoonHub

--- PyPy3/백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
+++ PyPy3/백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,26 +1,31 @@
-nums = []
-while 1:
-    N = int(input())
-    if not N:
-        break
-    else:
-        nums.append(N)
+# 소수인지 아닌지 확인하는 함수
+def is_prime(N):
+    for i in range(2, int(N**0.5)+1):
+        if not N%i:
+            return 0
+    return 1
 
-L = max(nums)
+# 골드바흐 검증 함수
+def goldbach():
+    import sys
+    input = sys.stdin.read
+    nums = input().splitlines()
+    nums = [int(num) for num in nums if num.strip().isdigit()]
+    nums.pop()
 
-primes = [0] * 2 + [1] * (L-1)
-for i in range(2, int(L**0.5)+1):
-    if primes[i]:
-        for j in range(i*2, L+1, i):
-            primes[j] = 0
+    L = max(nums)
+    primes = [0] * 2 + [1] * (L-1)
+    for i in range(2, int(L**0.5)+1):
+        if primes[i]:
+            for j in range(i*2, L+1, i):
+                primes[j] = False
 
-for num in nums:
-    gb = 3
-    while gb <= num//2:
-        if primes[gb] & primes[num-gb]:
-            print(f'{num} = {gb} + {num-gb}')
-            break
+    for num in nums:
+        for gb in range(3, num//2+1, 2):
+            if primes[gb] and primes[num-gb]:
+                print(f'{num} = {gb} + {num-gb}')
+                break
         else:
-            gb += 1
-    else:
-        print('Goldbach\'s conjecture is wrong.')
+            print("Goldbach's conjecture is wrong.")
+
+goldbach()
