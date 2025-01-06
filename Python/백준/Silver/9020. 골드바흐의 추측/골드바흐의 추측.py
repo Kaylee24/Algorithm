84c88f615707b1b4bfc79d9c9e3bb2d595237fc4
Songhee Kim <156345314+Kaylee24@users.noreply.github.com>
2025-01-07 02:00:21 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 428 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ Python/백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,35 +1,18 @@
-nums = []
+def is_prime(N):
+    if N == 1:
+        return False
+    for j in range(2, int(N**0.5)+1):
+        if N % j == 0:
+            return False
+    return True
 
 for _ in range(int(input())):
-    nums.append(int(input()))
+    num = int(input())
 
-L = max(nums)
-
-prime_check = [0] * (L + 1)
-for i in range(2, int(L ** 0.5) + 1):
-    if not prime_check[i]:
-        x = 2
-        while i * x <= L:
-            prime_check[i * x] = 1
-            x += 1
-
-# print('prime_check :', prime_check)
-
-prime_idx = []
-for i in range(2, L + 1):
-    if not prime_check[i]:
-        prime_idx.append(i)
-
-# print('prime_idx :', prime_idx)
-
-# 주어진 숫자들을 순회하면서
-for num in nums:
-    # 골드바흐 파티션 구하기
-    small_gb = num // 2
-    # print('small_gb :', small_gb)
+    small_gb = num//2
     while 1:
-        if (small_gb in prime_idx) & (num - small_gb in prime_idx):
-            print(small_gb, num - small_gb)
+        if is_prime(small_gb) and is_prime(num-small_gb):
+            print(small_gb, num-small_gb)
             break
         else:
             small_gb -= 1
