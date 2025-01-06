753ffe46d0818e57b7c43c1216c1ed10dd573483
Songhee Kim <156345314+Kaylee24@users.noreply.github.com>
2025-01-07 02:19:37 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 112 ms, Memory: 111540 KB -BaekjoonHub

--- PyPy3/백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ PyPy3/백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -7,12 +7,12 @@ for _ in range(int(input())):
 
 L = max(nums)
 
-prime_check = [False] * 2 + [True] * (L-1)
+prime_check = [0] * 2 + [1] * (L-1)
 
 for i in range(2, int(L**0.5)+1):
     if prime_check[i]:
         for j in range(2*i, L+1, i):
-            prime_check[j] = False
+            prime_check[j] = 0
 
 for num in nums:
     for i in range(num//2, 1, -1):
