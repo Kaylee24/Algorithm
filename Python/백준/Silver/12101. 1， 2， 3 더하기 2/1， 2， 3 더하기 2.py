799934f53e7b7b58442f1d4205f709b3c9b28065
Songhee Kim <156345314+Kaylee24@users.noreply.github.com>
2025-01-08 23:38:10 +0900

    [Silver I] Title: 1, 2, 3 더하기 2, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.py
+++ Python/백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.py
@@ -1,36 +1,13 @@
-import sys
-input = sys.stdin.readline
-
-dp = [0] * 11
-dp[1:4] = [1, 2, 4]
-
-for i in range(4, 11):
-    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
-
-result = []
-pre = [[], [[1]], [[1, 1], [2]], [[1, 1, 1], [1, 2], [2, 1], [3]]]
-
-
-def f(x, k):
-    global result
-
-    if x < 4:
-        if k:
-            result += pre[x][k-1]
-        return
-
-    temp = 0
-    for i in range(1, 4):
-        temp += dp[x-i]
-        if temp >= k:
-            result.append(i)
-            f(x-i, k-temp+dp[x-i])
-            break
-
-
 N, K = map(int, input().split())
-if K > dp[N]:
-    print(-1)
-else:
-    f(N, K)
-    print("+".join(map(str, result)))
+
+data = {1:["1"], 2:["1+1", "2"], 3:["1+2", "1+1+1", "2+1", "3"]}
+for i in range(4, 11):
+    data[i] = []
+    for j in data[i-1]:
+        data[i].append(j+"+1")
+    for j in data[i-2]:
+        data[i].append(j+"+2")
+    for j in data[i-3]:
+        data[i].append(j+"+3")
+
+print(sorted(data[N])[K-1] if len(data[N]) >= K else -1)
