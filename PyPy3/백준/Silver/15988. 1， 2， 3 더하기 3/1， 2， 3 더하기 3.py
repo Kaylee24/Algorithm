d24e5629f576244563039435b5082b4f5f3fd3b5
Songhee Kim <156345314+Kaylee24@users.noreply.github.com>
2025-01-08 23:54:55 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 140 ms, Memory: 184924 KB -BaekjoonHub

--- PyPy3/백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
+++ PyPy3/백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -1,6 +1,10 @@
+q = []
+for _ in range(int(input())):
+    q.append(int(input()))
+
 dp = [0, 1, 2, 4]
-for i in range(4, 1000001):
+for i in range(4, max(q)+1):
     dp.append((dp[i-1] + dp[i-2] + dp[i-3]) % 1000000009)
 
-for _ in range(int(input())):
-    print(dp[int(input())])
+for x in q:
+    print(dp[x])
