64944b4f1d5745145052d4f21d5c4f460f1e0590
Songhee Kim <156345314+Kaylee24@users.noreply.github.com>
2025-02-01 22:31:43 +0900

    [Bronze V] Title: A+B, Time: 100 ms, Memory: 14240 KB -BaekjoonHub

--- Java/백준/Bronze/1000. A＋B/A＋B.java
+++ Java/백준/Bronze/1000. A＋B/A＋B.java
@@ -3,20 +3,16 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
 
-			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-			int n = Integer.parseInt(st.nextToken());
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
-		int m = Integer.parseInt(st.nextToken());
-
-		System.out.print(n+m);
-
-		br.close();
-	}
+        System.out.println(a + b);
+    }
 }
