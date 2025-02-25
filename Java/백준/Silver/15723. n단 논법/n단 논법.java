9ea969595aa31a396e48a624224892991fa51f11
Songhee Kim <156345314+Kaylee24@users.noreply.github.com>
2025-02-25 23:20:19 +0900

    [Silver I] Title: n단 논법, Time: 100 ms, Memory: 14100 KB -BaekjoonHub

---
+++ Java/백준/Silver/15723. n단 논법/n단 논법.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        boolean[][] connected = new boolean[26][26];
+
+        while (N-->0) {
+            String[] str = br.readLine().split(" is ");
+            char a = str[0].charAt(0);
+            char b = str[1].charAt(0);
+
+            connected[a - 'a'][b - 'a'] = true;
+        }
+
+        for (int k=0; k<26; k++) {
+            for (int i=0; i<26; i++) {
+                for (int j=0; j<26; j++) {
+                    if (i==j || j==k || k==i) continue;
+
+                    if (connected[i][k] && connected[k][j]) {
+                        connected[i][j] = true;
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int M = Integer.parseInt(br.readLine());
+        while (M-->0) {
+            String[] str = br.readLine().split(" is ");
+            char a = str[0].charAt(0);
+            char b = str[1].charAt(0);
+
+            sb.append(connected[a - 'a'][b - 'a'] ? "T" : "F");
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
