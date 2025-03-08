45fa968e0fe2301e1785bb2d14a0f6ca1b8e00c8
Songhee Kim <156345314+Kaylee24@users.noreply.github.com>
2025-03-08 23:57:18 +0900

    [Silver II] Title: 초콜릿 식사, Time: 116 ms, Memory: 15700 KB -BaekjoonHub

---
+++ Java/백준/Silver/2885. 초콜릿 식사/초콜릿 식사.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+        int size = 1;
+        int cnt = 0;
+
+        while (size < K) size *= 2;
+        System.out.print(size + " ");
+
+        while (K > 0) {
+            if (K >= size) {
+                K -= size;
+            } else {
+                size /= 2;
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
