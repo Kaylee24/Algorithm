58cfb194a65fd482c036f78cdb84458f00eeda0d
Songhee Kim <156345314+Kaylee24@users.noreply.github.com>
2025-06-21 23:01:16 +0900

    [Bronze III] Title: 대지, Time: 144 ms, Memory: 111328 KB -BaekjoonHub

---
+++ PyPy3/백준/Bronze/9063. 대지/대지.py
@@ -0,0 +1,14 @@
+x1, x2, y1, y2 = 10000, -10000, 10000, -10000
+
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    if x < x1:
+        x1 = x
+    if x > x2:
+        x2 = x
+    if y < y1:
+        y1 = y
+    if y > y2:
+        y2 = y
+
+print((x2-x1)*(y2-y1))
